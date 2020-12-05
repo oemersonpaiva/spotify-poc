666949a56943d6208b8acde77b193537b37488f4
Emerson Paiva <emerson@pop-os.localdomain>
2020-12-05 11:48:10 -0300

    feat: add storybook and jest ThemeProvider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' }
 }
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      {Story()}
+    </ThemeProvider>
+  )
+]
